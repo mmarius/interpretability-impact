c2bb48576db18469b0a2977213b890001af603eb
Marius Mosbach <mosbach.marius@gmail.com>
2023-11-23 22:10:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Impact of interpretability and analysis research
+
+## Setup
+
+1. Create new conda environment: `conda create -n interpretability-impact python=3.11`
+2. Install dependencies in conda environment: `pip install -r ./requirements.txt`
+
+## Notes
+
+- Querying all citations for a given paper can take several minutes at the moment
+
+## TODOs
+
+- [ ] Speed up citation graph construction
+- [ ]  
---
+++ utils.py
@@ -0,0 +1,233 @@
+from acl_anthology import Anthology
+# Instantiate the Anthology from the official repository
+# This will download the Anthology once at the beginning
+anthology = Anthology.from_repo()
+
+import pandas as pd
+from pandas import DataFrame
+import requests
+from tqdm import tqdm
+
+API_KEY = "your-api-key" # TODO(mm): remove when making repo public
+
+# Rate limit:
+#   1 request per second for the following endpoints:
+#       /paper/batch
+#       /paper/search
+#       /recommendations
+#   10 requests / second for all other calls
+
+#########################################################
+# Paper utils
+#########################################################
+
+class SemanticScholarPaper:
+    """A Semantic Scholar paper"""
+
+    paper_id: str = None
+    title: str = None
+    venue: str = None
+    # venue_details: dict = None
+    year: int = None
+    citation_count: int = None
+    influential_citation_count: int = None
+    acl_doi: str = None
+
+    # additional attributes that will be filled when building a citation graph
+    citations: list = []
+    cites_result: bool = False
+    cites_methodology: bool = False
+    cites_background: bool = False
+    was_influenced: bool = False
+    contexts: list[str] = []
+
+    def __init__(self, paper_id: str, title: str, venue: str, year: int, citation_count: int, influential_citation_count: int):
+        self.paper_id = paper_id
+        self.title = title
+        self.venue = venue
+        self.year = year
+        self.citation_count = citation_count
+        self.influential_citation_count = influential_citation_count
+
+    def add_acl_doi(self, doi: str) -> None:
+        self.acl_doi = doi
+
+    def add_citation_types(self, cites_result: bool, cites_methodology: bool, cites_background: bool):
+        self.cites_result = cites_result
+        self.cites_methodology = cites_methodology
+        self.cites_background = cites_background
+
+    def add_was_influenced(self, was_influenced: bool):
+        self.was_influenced = was_influenced
+
+    def add_contexts(self, contexts: list[str]):
+        self.contexts = contexts
+
+    def retrieve_citations(self, limit: int = 1000) -> None:
+        # get citing papers from semantic scholar api
+        citing_papers = get_citation_details(self.paper_id, limit)
+        citing_papers = [merge_into_single_dict(d) for d in citing_papers['data']]
+
+        for paper_dict in tqdm(citing_papers, desc="Collecting citations"):
+            paper = get_paper_details(paper_dict["citingPaper_paperId"])
+            paper.add_citation_types(
+                cites_result=paper_dict["result"],
+                cites_background=paper_dict["background"],
+                cites_methodology=paper_dict["methodology"]
+            )
+            paper.add_was_influenced(paper_dict["isInfluential"])
+            paper.add_contexts(paper_dict["contexts"])
+            self.citations.append(paper)
+
+    def to_dict(self):
+        return vars(self)
+    
+    def to_df(self):
+        paper_dict = self.to_dict()
+        paper_dict = {k: [v] for k, v in paper_dict.items()}
+        return pd.DataFrame.from_dict(paper_dict, orient="columns")
+    
+    def citations_to_df(self):
+        columns = self.citations[0].to_df().columns
+        citations_dict = {k: [] for k in columns}
+        
+        for paper in self.citations:
+            for k, v in paper.to_dict().items():
+                citations_dict[k].append(v)
+
+        return pd.DataFrame.from_dict(citations_dict, orient="columns")
+    
+    def __str__(self):
+        return str(self.to_dict())
+
+
+#########################################################
+# ACL Anthology related utils
+#########################################################
+
+def get_acl_anthology_doi(title: str) -> str | None:
+    """
+    Return the DOI of an ACL Anthology paper.
+
+            Parameters:
+                    title (str): The title of the paper
+
+            Returns:
+                    doi (str): The DOI of the paper or None of the paper is not found in the ACL Anthology
+    """
+    for paper in anthology.papers():        
+        if title.lower() == str(paper.title).lower():
+            return paper.doi
+
+    return None
+
+
+#########################################################
+# Semantic Scholar related utils
+#########################################################
+
+
+def get_papers_by_keywords(keywords: str, limit: int = 100) -> dict:
+    # return a dictionary of papers 
+    query = f"https://api.semanticscholar.org/graph/v1/paper/search?query={keywords}&offset=0&limit={limit}"
+    fields = "paperId,title,year,venue"
+
+    # query Semantic Scholar API
+    response = requests.get(query, headers={"x-api-key": API_KEY}, params={"fields": fields})
+    while response.status_code != 200: # 200 means success
+        # try again
+        print("Trying again ...")
+        response = requests.get(query, headers={"x-api-key": API_KEY}, params={"fields": fields})
+
+    # Success! Convert response to dict 
+    papers = response.json()
+
+    # TODO(mm): convert to paper class
+
+    return papers
+
+def get_paper_details(paper_id: str) -> SemanticScholarPaper:
+    # get details for a single paper
+    query = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}"
+    fields = "title,venue,year,citationCount,influentialCitationCount"
+    
+    # query Semantic Scholar API
+    response = requests.get(query, headers={"x-api-key": API_KEY}, params={"fields": fields})
+    while response.status_code != 200: # 200 means success
+        # try again
+        print("Trying again ...")
+        response = requests.get(query, headers={"x-api-key": API_KEY}, params={"fields": fields})
+
+    # Success! Convert response to dict 
+    paper_dict = response.json()
+
+    paper = SemanticScholarPaper(
+        paper_id=paper_id, 
+        title=paper_dict["title"],
+        venue=paper_dict["venue"],
+        year=paper_dict["year"],
+        citation_count=paper_dict["citationCount"],
+        influential_citation_count=paper_dict["influentialCitationCount"],
+    )
+    return paper
+
+def get_citation_details(paper_id: str, limit: int = 1000) -> dict:
+    # get papers that cite a paper 
+    query = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}/citations"
+    # fields = "title,isInfluential,contexts,intents,year,venue,citationCount,influentialCitationCount"
+    fields = "title,isInfluential,contexts,intents"
+    
+    # query Semantic Scholar API
+    response = requests.get(query, headers={"x-api-key": API_KEY}, params={"fields": fields, "limit": f"{limit}"})
+    while response.status_code != 200: # 200 means success
+        # try again
+        print("Trying again ...")
+        response = requests.get(query, headers={"x-api-key": API_KEY}, params={"fields": fields, "limit": f"{limit}"})
+
+    # Success! Convert response to dict 
+    details = response.json()
+    
+    # add paper_id to each of the citing papers
+    for p in details['data']:
+        p['paperId'] = paper_id
+
+    return details 
+
+#########################################################
+# pandas related utils
+#########################################################
+
+def convert_to_df(papers: dict) -> DataFrame:
+    df = DataFrame.from_dict(papers, orient='columns')
+    return df
+
+
+def filter_df(df, **kwargs):
+    for column, value in kwargs.items():
+        mask = None
+        if column in df.columns:
+            if mask is None:
+                mask = (df[column] == value).values
+            else:
+                mask = mask & (df[column] == value).values
+        else:
+            print(f"Ignoring argument {column} with value {value}")
+    
+    return df[mask]
+
+#########################################################
+# Misc.
+#########################################################
+
+def merge_into_single_dict(d: dict) -> dict:
+    r = {'result': False, 'methodology': False, 'background': False}
+    for k, v in d.items():
+        if k == 'citingPaper':
+            for kk in v:
+                r[f'{k}_{kk}'] = v[kk]
+        elif k == 'intents': # convert list intents into single columns
+            for intent in v:
+                r[intent] = True
+        else:
+            r[k] = v
+    return r
